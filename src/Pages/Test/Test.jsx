a8a8a5a10ce990eb8fcbedcfe2ca60fe5d101bb5
S.Zelensky <zelenstein@gmail.com>
2024-01-22 19:28:24 +0300

    test

--- src/Pages/Test/Test.jsx
+++ src/Pages/Test/Test.jsx
@@ -4,17 +4,6 @@ import s from './Test.module.scss';
 const Test = () => {
   return (
     <div className={s.test}>
-      <div>12,502 people did this test last week.</div>
-      <div>Test your IQ!</div>
-      <div>
-        The IQ Test is to assess an intellectual abilities formed by general
-        experience, not affected by training or learning. You can figure out
-        your intellectual level compared to the group of the same age.
-      </div>
-      <div>
-        As it requires concentration for a given amount of time, please conduct
-        the test in a quiet and comfortable place.
-      </div>
       <button>Start IQ test</button>
     </div>
   );
