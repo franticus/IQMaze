122b04beb65fab0d5aaacada9fbe69dd76802fce
S.Zelensky <zelenstein@gmail.com>
2024-06-07 11:36:32 +0300

    publicKeyDEV

--- src/Pages/Paywall/CheckoutButton.jsx
+++ src/Pages/Paywall/CheckoutButton.jsx
@@ -1,17 +1,15 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 
+import { publicKey, publicKeyDEV, url, urlDEV } from '../../key.js';
+
 const CheckoutButton = () => {
   const [stripe, setStripe] = useState(null);
-  const url = 'https://iqmazestripe-myfirst27.amvera.io';
-  const publicKey =
-    'pk_live_51Huk90BbDeRYiB9tviB7TIaYaMB0uYOyK7wIPE6Q4LNOhuSyJTY7rxW9M30YFkIOp2RDOngiLmGnp5uBh00EirHF00tQNCRo3i';
-  // const publicKeyDEV =
-  //   'pk_test_51PNcn6RrQfUQC5MYaOchK1YrrDtBrxRDbyzQ2rfUIw7QhiIPmOU0vLYBq17pyMSQKAw99bqVnmeYGELIq2KOncST00ysRkRCO0';
 
   React.useEffect(() => {
     const initializeStripe = async () => {
-      const stripe = await loadStripe(publicKey);
+      const stripe = await loadStripe(publicKeyDEV);
       setStripe(stripe);
     };
     initializeStripe();
