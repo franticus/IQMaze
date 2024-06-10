fd049323489b86030cf3cd714a270ee252be1e37
S.Zelensky <zelenstein@gmail.com>
2024-06-10 19:51:54 +0300

    key

--- src/key.js
+++ src/key.js
@@ -1,7 +1,7 @@
 export const publicKey =
   'pk_live_51Huk90BbDeRYiB9tviB7TIaYaMB0uYOyK7wIPE6Q4LNOhuSyJTY7rxW9M30YFkIOp2RDOngiLmGnp5uBh00EirHF00tQNCRo3i';
 export const publicKeyDEV =
-  'pk_test_51PNcn6RrQfUQC5MYaOchK1YrrDtBrxRDbyzQ2rfUIw7QhiIPmOU0vLYBq17pyMSQKAw99bqVnmeYGELIq2KOncST00ysRkRCO0';
+  'pk_test_51Huk90BbDeRYiB9tr54u7lgp6s8tMKhREIFPLV6jCqZh65REX5WKn4Gtjk1b07qaHFaxDScN3OX4nVj4xMA3iYNm00janhu0oE';
 export const url = 'https://iqmazestripe-myfirst27.amvera.io';
 export const urlDEV = 'https://stripeiq-frantunn.amvera.io';
 export const urlLOCAL = 'http://localhost:4242';
