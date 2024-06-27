550d2933898cda6e502f4aa4108bb5fa63518d52
S.Zelensky <zelenstein@gmail.com>
2024-06-27 12:16:48 +0300

    HeroSection

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,12 +2,11 @@ import React, { useEffect, useState } from 'react';
 import s from './Home.module.scss';
 import useCustomNavigate from '../../hooks/useCustomNavigate';
 import Testimonials from '../../components/Testimonials/Testimonials';
-import cn from 'classnames';
 import { checkSubscription } from '../../helpers/stripeHelpers';
 import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
+import HeroSection from '../../components/HeroSection/HeroSection'; // Импортируем новый компонент
 
-import about_1 from '../../img/about_1.jpg';
 import about_2 from '../../img/about_2.jpg';
 import about_3 from '../../img/about_3.jpg';
 import about_4 from '../../img/about_4.jpg';
@@ -78,69 +77,13 @@ const Home = ({ user }) => {
 
   return (
     <div className={s.home}>
-      <div className={s.hero}>
-        {loading ? (
-          <Skeleton height={20} width={200} />
-        ) : (
-          <div className={s.did}>12,502 people did this test last week.</div>
-        )}
-        <div className={s.title}>
-          Test your <span className={s.title_iq}>IQ!</span>
-        </div>
-        {loading ? (
-          <>
-            <Skeleton count={2} />
-            <Skeleton height={40} width={200} className={s.buttonSkeleton} />
-          </>
-        ) : (
-          <>
-            <div className={s.description}>
-              The IQ Test is to assess intellectual abilities formed by general
-              experience, not affected by training or learning. You can figure
-              out your intellectual level compared to the group of the same age.
-            </div>
-            <div className={s.description}>
-              As it requires concentration for a given amount of time, please
-              conduct the test in a quiet and comfortable place.
-            </div>
-            <button className={s.button} onClick={handleStartTest}>
-              {hasStartedTest ? 'Continue IQ test' : 'Start IQ test'}
-            </button>
-            {showLastResults && (
-              <button
-                className={cn(s.button, s.button_last)}
-                onClick={handleShowLastResults}
-              >
-                Show Last Results
-              </button>
-            )}
-          </>
-        )}
-      </div>
-
-      <section className={s.heroSection}>
-        <div className={s.heroContent}>
-          <h1 className={s.mainHeading}>
-            Unlock Your Cognitive Potential with IQMaze
-          </h1>
-          <p className={s.introText}>
-            Join us on an exciting journey through the realms of intelligence
-            and cognitive prowess. IQMaze offers scientifically validated tests
-            that unveil the intricacies of your mind, helping you discover your
-            unique intellectual strengths.
-          </p>
-          {loading ? (
-            <Skeleton height={300} width={300} />
-          ) : (
-            <img
-              src={about_1}
-              alt='Brain maze representing the complexity of human intelligence'
-              className={s.heroImage}
-              loading='lazy'
-            />
-          )}
-        </div>
-      </section>
+      <HeroSection
+        loading={loading}
+        hasStartedTest={hasStartedTest}
+        showLastResults={showLastResults}
+        handleStartTest={handleStartTest}
+        handleShowLastResults={handleShowLastResults}
+      />
 
       <section className={s.featuresSection}>
         <h2 className={s.sectionHeading}>Why Choose IQMaze?</h2>
