fefc852014287c341f14470e421f857a065543bc
S.Zelensky <zelenstein@gmail.com>
2024-07-08 11:55:17 +0300

    Navbar & Footer hide

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,39 +12,53 @@ const Footer = () => {
   const customNavigate = useCustomNavigate();
   const currentUrl = window.location.href;
   const isPaywall = currentUrl.includes('paywall');
+  const isPrestart = currentUrl.includes('prestart');
+  const isIQTest = currentUrl.includes('iqtest');
+  const isHide = isPrestart || isIQTest;
 
   return (
-    <footer className={cn(s.footer, isPaywall && s.footer_pb)}>
-      <div className={s.footer__container}>
-        <div className={s.logo}>
-          <img className={s.mainLogo} src={logo} alt='IQMaze Logo' />
-          <span className={s.logoText} onClick={() => customNavigate('/home')}>
-            IQMaze
-          </span>
-        </div>
-        <div className={s.payImgs_container}>
-          <img className={s.payImgs} src={visa} alt='visa' />
-          <img className={s.payImgs} src={mastercard} alt='mastercard' />
-          <img className={s.payImgs} src={amex} alt='amex' />
-          <img className={s.payImgs} src={discover} alt='discover' />
-        </div>
-        <div className={s.links}>
-          <span className={s.link} onClick={() => customNavigate('/privacy')}>
-            Privacy Policy
-          </span>
-          <span className={s.link} onClick={() => customNavigate('/terms')}>
-            Terms of Use
-          </span>
-          <a href='mailto:admin@iq-check140.com' className={s.link}>
-            Contact Us
-          </a>
-        </div>
-        <div className={s.copyright}>
-          © <span onClick={() => customNavigate('/home')}>IQMaze 2024.</span>{' '}
-          All rights reserved
-        </div>
-      </div>
-    </footer>
+    <>
+      {!isHide && (
+        <footer className={cn(s.footer, isPaywall && s.footer_pb)}>
+          <div className={s.footer__container}>
+            <div className={s.logo}>
+              <img className={s.mainLogo} src={logo} alt='IQMaze Logo' />
+              <span
+                className={s.logoText}
+                onClick={() => customNavigate('/home')}
+              >
+                IQMaze
+              </span>
+            </div>
+            <div className={s.payImgs_container}>
+              <img className={s.payImgs} src={visa} alt='visa' />
+              <img className={s.payImgs} src={mastercard} alt='mastercard' />
+              <img className={s.payImgs} src={amex} alt='amex' />
+              <img className={s.payImgs} src={discover} alt='discover' />
+            </div>
+            <div className={s.links}>
+              <span
+                className={s.link}
+                onClick={() => customNavigate('/privacy')}
+              >
+                Privacy Policy
+              </span>
+              <span className={s.link} onClick={() => customNavigate('/terms')}>
+                Terms of Use
+              </span>
+              <a href='mailto:admin@iq-check140.com' className={s.link}>
+                Contact Us
+              </a>
+            </div>
+            <div className={s.copyright}>
+              ©{' '}
+              <span onClick={() => customNavigate('/home')}>IQMaze 2024.</span>{' '}
+              All rights reserved
+            </div>
+          </div>
+        </footer>
+      )}
+    </>
   );
 };
 
