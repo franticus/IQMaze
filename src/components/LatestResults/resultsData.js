67d4f5084a8c9f1a5f3ce4b3b01cfa142fa86ce9
S.Zelensky <zelenstein@gmail.com>
2024-07-19 14:13:06 +0300

    flags

--- src/components/LatestResults/resultsData.js
+++ src/components/LatestResults/resultsData.js
@@ -1,151 +1,161 @@
+import AR from '../../img/flags/AR.png';
+import BR from '../../img/flags/BR.png';
+import CA from '../../img/flags/CA.png';
+import DE from '../../img/flags/DE.png';
+import ES from '../../img/flags/ES.png';
+import FR from '../../img/flags/FR.png';
+import IN from '../../img/flags/IN.png';
+import RU from '../../img/flags/RU.png';
+import US from '../../img/flags/US.png';
+
 export const resultsData = [
   {
-    flag: '../../img/flags/AR.png',
+    flag: AR,
     name: 'Martínez Juan',
     iqScore: 91,
   },
   {
-    flag: '../../img/flags/BR.png',
+    flag: BR,
     name: 'Silva Maria',
     iqScore: 102,
   },
   {
-    flag: '../../img/flags/AR.png',
+    flag: AR,
     name: 'González Carlos',
     iqScore: 97,
   },
   {
-    flag: '../../img/flags/IN.png',
+    flag: IN,
     name: 'Patel Ananya',
     iqScore: 108,
   },
   {
-    flag: '../../img/flags/CA.png',
+    flag: CA,
     name: 'Smith John',
     iqScore: 105,
   },
   {
-    flag: '../../img/flags/IN.png',
+    flag: IN,
     name: 'Rodríguez Isabella',
     iqScore: 99,
   },
   {
-    flag: '../../img/flags/DE.png',
+    flag: DE,
     name: 'Müller Hans',
     iqScore: 103,
   },
   {
-    flag: '../../img/flags/FR.png',
+    flag: FR,
     name: 'Dubois Emma',
     iqScore: 95,
   },
   {
-    flag: '../../img/flags/IN.png',
+    flag: IN,
     name: 'Hernández Luis',
     iqScore: 98,
   },
   {
-    flag: '../../img/flags/IN.png',
+    flag: IN,
     name: 'Rossi Giulia',
     iqScore: 104,
   },
   {
-    flag: '../../img/flags/IN.png',
+    flag: IN,
     name: 'Martínez Sofia',
     iqScore: 93,
   },
   {
-    flag: '../../img/flags/RU.png',
+    flag: RU,
     name: 'Иванов Алексей',
     iqScore: 110,
   },
   {
-    flag: '../../img/flags/ES.png',
+    flag: ES,
     name: 'García Miguel',
     iqScore: 92,
   },
   {
-    flag: '../../img/flags/IN.png',
+    flag: IN,
     name: 'Kask Mari',
     iqScore: 99,
   },
   {
-    flag: '../../img/flags/RU.png',
+    flag: RU,
     name: 'Korhonen Mika',
     iqScore: 105,
   },
   {
-    flag: '../../img/flags/PL.png',
+    flag: CA,
     name: 'Nowak Anna',
     iqScore: 97,
   },
   {
-    flag: '../../img/flags/RU.png',
+    flag: RU,
     name: 'Papadopoulos Nikos',
     iqScore: 94,
   },
   {
-    flag: '../../img/flags/RU.png',
+    flag: RU,
     name: 'Johansson Lars',
     iqScore: 101,
   },
   {
-    flag: '../../img/flags/RU.png',
+    flag: RU,
     name: 'Nagy Bence',
     iqScore: 102,
   },
   {
-    flag: '../../img/flags/US.png',
+    flag: US,
     name: 'Meier Lena',
     iqScore: 106,
   },
   {
-    flag: '../../img/flags/UY.png',
+    flag: US,
     name: 'González Mateo',
     iqScore: 96,
   },
   {
-    flag: '../../img/flags/US.png',
+    flag: US,
     name: 'Petrović Luka',
     iqScore: 93,
   },
   {
-    flag: '../../img/flags/US.png',
+    flag: US,
     name: 'Novak Anja',
     iqScore: 98,
   },
   {
-    flag: '../../img/flags/US.png',
+    flag: US,
     name: 'García Valentina',
     iqScore: 95,
   },
   {
-    flag: '../../img/flags/US.png',
+    flag: US,
     name: 'Hernández Felipe',
     iqScore: 90,
   },
   {
-    flag: '../../img/flags/IN.png',
+    flag: IN,
     name: 'Ramírez Camila',
     iqScore: 87,
   },
   {
-    flag: '../../img/flags/US.png',
+    flag: US,
     name: 'Vega Laura',
     iqScore: 92,
   },
   {
-    flag: '../../img/flags/IN.png',
+    flag: IN,
     name: 'Martínez Diego',
     iqScore: 100,
   },
   {
-    flag: '../../img/flags/US.png',
+    flag: US,
     name: 'Brown Jaden',
     iqScore: 95,
   },
   {
-    flag: '../../img/flags/US.png',
+    flag: US,
     name: 'Johnson Emily',
     iqScore: 105,
   },
